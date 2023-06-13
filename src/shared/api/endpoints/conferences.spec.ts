8f81cd1df286ef9ff12e0d3219ff696fd81bf653
7SilviaT <eagles8485@gmail.com>
2023-06-13 18:36:33 -0500

    test: Move mockResult inside test block

--- src/shared/api/endpoints/conferences.spec.ts
+++ src/shared/api/endpoints/conferences.spec.ts
@@ -2,32 +2,30 @@ import ConferenceAPI from './conferences'
 import { requests } from '../../../api/baseRequest'
 
 const mockResult = {
-  data: [
-    {
-      id: '01',
-      name: 'LinuxCon',
-      eventDate: '2021-12-28',
-      status: 'created',
-    },
-    {
-      id: '02',
-      name: 'KubernetesCon',
-      eventDate: '2022-10-08',
-      status: 'created',
-    },
-  ],
-}
-jest.mock('../../../api/baseRequest', () => ({
-  requests: {
-    get: () => jest.fn().mockResolvedValue(mockResult),
+  data: {
+    data: [
+      {
+        id: '01',
+        name: 'LinuxCon',
+        eventDate: '2021-12-28',
+        status: 'created',
+      },
+      {
+        id: '02',
+        name: 'KubernetesCon',
+        eventDate: '2022-10-08',
+        status: 'created',
+      },
+    ],
   },
-}))
+}
 
 describe('events api call', () => {
   it('should get all', async () => {
     const events = ConferenceAPI()
+    requests.get = jest.fn().mockResolvedValue(mockResult)
     const result = await events.getAll()
     expect(requests.get).toHaveBeenCalledWith('events')
-    expect(result).toBe(mockResult.data)
+    expect(result).toEqual(mockResult.data.data)
   })
 })
