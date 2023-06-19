8a56b8817fc7774e3d06d9abcc0a0c2e2d572cf5
7SilviaT <eagles8485@gmail.com>
2023-06-19 15:48:23 -0500

    test: Replace slash bars for strings in getByTestId assertions

--- src/pages/EventInfo/EventInfo.spec.tsx
+++ src/pages/EventInfo/EventInfo.spec.tsx
@@ -5,13 +5,13 @@ import EventInfoPage from './EventInfo'
 describe('Event Info Page', () => {
   it('should render component', () => {
     render(<EventInfoPage />)
-    const title = screen.getByText(/Fluent React/i)
+    const title = screen.getByText(/Fluent React/)
     expect(title).toBeInTheDocument()
 
-    const registerButton = screen.getByTestId(/register-button/)
+    const registerButton = screen.getByTestId('register-button')
     expect(registerButton).toBeInTheDocument()
 
-    const imageContainer = screen.getByTestId(/image-list-container/)
+    const imageContainer = screen.getByTestId('image-list-container')
     expect(imageContainer).toBeInTheDocument()
   })
 })
